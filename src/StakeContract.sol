08cfe5bde8b0521a89ac4b14c50412a35d56ee83
Michael Harris <mwharris7@gmail.com>
2023-12-28 21:45:31 -0700

    Finished writing simple staking contract + started writing tests

--- src/StakeContract.sol
+++ src/StakeContract.sol
@@ -2,9 +2,34 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract StakeContract {
+    mapping(address => mapping(address => uint256)) public s_balances;
 
+    error TransferFailed();
 
+    function stake(uint256 _amount, address token) external returns (bool) {
+        s_balances[msg.sender][token] += _amount;
+        bool success = IERC20(token).transferFrom(
+            msg.sender,
+            address(this),
+            _amount
+        );
+        if (!success) revert TransferFailed();
+        return success;
+    }
+
+    // Function Brainstorming:
+    // ------------------------------------------------------------
+    // function unstake(uint256 _amount) public {}
+    // function withdraw(uint256 _amount) public {}
+    // function claimReward() public {}
+    // function exit() public {}
+    // function getReward() public {}
+    // function getStakeBalance() public view returns(uint256) {}
+    // function getRewardBalance() public view returns(uint256) {}
+    // function getStakeCount() public view returns(uint256) {}
+    // function getRewardCount() public view returns(uint256) {}
+    // function getStakeInfo(uint256 _index) public view returns(address, uint256, uint256, uint2
 }
--- test/StakeContract.t.sol
+++ test/StakeContract.t.sol
@@ -1,3 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "ds-test/test.sol";
+
+contract StakeContractTest is DSTest {
+    function setUp() public {}
+
+    function test_stake_contract() public {
+        assertTrue(true);
+    }
+}
